07cda3035e546799cf646dd57ea563dcf9f3d8f5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-01 22:39:41 -0500

    Swap base face & create auto indices

--- source/blockmodel/blockmodel.d
+++ source/blockmodel/blockmodel.d
@@ -9,7 +9,13 @@ import vector_3i;
 
 class BlockModel {
 
+    private immutable int[] indiceOrder = [
+        0,1,3,3,1,2
+    ];
+
     Vector3d size = Vector3d(1,1,1);
+
+    int count = 0;
     
     float[] vertexPositions;
     int[] indices;
@@ -25,43 +31,46 @@ class BlockModel {
     void constructVertexPositions() {
         
         // 8 Vertex Positions - written extremely verbosely
-        // Wall 1 (bottom)
-        vertexPositions ~= -size.x;
-        vertexPositions ~= -size.y;
-        vertexPositions ~= -size.z;
-
+        // Wall 1 (FRONT)
         vertexPositions ~= -size.x;
         vertexPositions ~= -size.y;
         vertexPositions ~= size.z;
 
-        vertexPositions ~= size.x;
-        vertexPositions ~= -size.y;
-        vertexPositions ~= -size.z;
-
-        vertexPositions ~= size.x;
-        vertexPositions ~= -size.y;
-        vertexPositions ~= size.z;
-
-        // Wall 2 (top)
         vertexPositions ~= -size.x;
         vertexPositions ~= size.y;
+        vertexPositions ~= size.z;
+
+        vertexPositions ~= size.x;
+        vertexPositions ~= -size.y;
+        vertexPositions ~= size.z;
+
+        vertexPositions ~= size.x;
+        vertexPositions ~= size.y;
+        vertexPositions ~= size.z;
+
+        // Wall 2 (BACK)
+        vertexPositions ~= -size.x;
+        vertexPositions ~= -size.y;
         vertexPositions ~= -size.z;
         
-        vertexPositions ~= size.x;
+        vertexPositions ~= -size.x;
         vertexPositions ~= size.y;
         vertexPositions ~= -size.z;
 
-        vertexPositions ~= -size.x;
-        vertexPositions ~= size.y;
-        vertexPositions ~= size.z;
+        vertexPositions ~= size.x;
+        vertexPositions ~= -size.y;
+        vertexPositions ~= -size.z;
 
         vertexPositions ~= size.x;
         vertexPositions ~= size.y;
-        vertexPositions ~= size.z;
+        vertexPositions ~= -size.z;
     }
 
     void constructIndices() {
-        
+        const currentCount = indices.length;
+        foreach (int key; indiceOrder) {
+            indices ~= currentCount + key;
+        }
     }
 
     float[] getVertexPositions() {
