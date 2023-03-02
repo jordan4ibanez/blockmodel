babf35b884417cbaff0f0d67e75257a5fc830353
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-01 22:12:26 -0500

    Begin again

---
+++ source/blockmodel/blockmodel.d
@@ -0,0 +1,12 @@
+module blockmodel.blockmodel;
+
+import std.stdio;
+import std.algorithm.sorting;
+import std.conv;
+import tinygltf;
+import vector_3d;
+import vector_3i;
+
+class BlockModel {
+    
+}
