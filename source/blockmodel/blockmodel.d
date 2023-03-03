4dd1c859119ed2429c17b51367f5cd457272e727
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-02 19:24:51 -0500

    Separators

--- source/blockmodel/blockmodel.d
+++ source/blockmodel/blockmodel.d
@@ -61,6 +61,159 @@ class BlockModel {
         this.loadBlocks(fileLocation);
     }
 
+    //*========================= BEGIN OPENGL METHODS =====================================
+    
+
+    void constructCube(double width, double height, double length) {
+
+        this.size.x = width;
+        this.size.y = height;
+        this.size.z = length;
+
+        this.constructVertexPositions();
+        this.constructIndices();
+    }
+
+    void constructVertexPositions() {
+        
+        // 8 Vertex Positions
+
+        // Wall 1 (FRONT)
+        const auto v0 = Vector3d(size.x, size.y * 2, -size.z);
+        const auto v1 = Vector3d(size.x, 0, -size.z);
+        const auto v2 = Vector3d(-size.x, 0, -size.z);
+        const auto v3 = Vector3d(-size.x, size.y * 2, -size.z);
+
+        // Wall 2 (BACK)
+        const auto v4 = Vector3d(-size.x, size.y * 2, size.z);
+        const auto v5 = Vector3d(-size.x, 0, size.z);
+        const auto v6 = Vector3d(size.x, 0, size.z);
+        const auto v7 = Vector3d(size.x, size.y * 2, size.z);
+
+        // Front face
+        assembleQuad(v0,v1,v2,v3);
+        //Back face
+        assembleQuad(v4,v5,v6,v7);
+
+        //Left face
+        assembleQuad(v3,v2,v5,v4);
+        //Right face
+        assembleQuad(v7,v6,v1,v0);
+
+        // Top face (up is -Z, points to front face)
+        assembleQuad(v3,v4,v7,v0);
+        // Bottom face (up is -Z, points to front face)
+        assembleQuad(v1,v6,v5,v2);
+
+        
+    }
+
+    // Builds a plane of 2 tris out of 4 vertex positions
+    void assembleQuad(Vector3d pos1, Vector3d pos2, Vector3d pos3, Vector3d pos4) {
+        foreach (thisVertexPos; [pos1, pos2, pos3, pos4]) {
+            vertexPositions ~= thisVertexPos.x;
+            vertexPositions ~= thisVertexPos.y;
+            vertexPositions ~= thisVertexPos.z;
+        }
+    }
+
+    // Assembles the indices of the block
+    void constructIndices() {
+
+        const int currentCount = cast(int)indices.length;
+
+        foreach (int key; indiceOrder) {
+            indices ~= currentCount + key;
+        }
+
+        writeln(this.indices);
+    }
+
+    float[] getVertexPositions() {
+        return this.vertexPositions;
+    }
+
+    int[] getIndices() {
+        return this.indices;
+    }
+
+    float[] getTextureCoordinates() {
+        // These are place holders for future modeling implementation
+        const float xMin = 0.0;
+        const float xMax = 1.0;
+        const float yMin = 0.0;
+        const float yMax = 1.0;
+        return [
+            //* Front face
+            // Top left
+            xMin,yMin,
+            // Bottom left
+            xMin,yMax,
+            // Top right
+            xMax,yMax,
+            // Bottom right
+            xMax,yMin,
+
+            //* Back face
+            // Top left
+            xMin,yMin,
+            // Bottom left
+            xMin,yMax,
+            // Top right
+            xMax,yMax,
+            // Bottom right
+            xMax,yMin,
+
+            //* Left face
+            // Top left
+            xMin,yMin,
+            // Bottom left
+            xMin,yMax,
+            // Top right
+            xMax,yMax,
+            // Bottom right
+            xMax,yMin,
+
+            //* Right face
+            // Top left
+            xMin,yMin,
+            // Bottom left
+            xMin,yMax,
+            // Top right
+            xMax,yMax,
+            // Bottom right
+            xMax,yMin,
+
+            //* Top face
+            // Top left
+            xMin,yMin,
+            // Bottom left
+            xMin,yMax,
+            // Top right
+            xMax,yMax,
+            // Bottom right
+            xMax,yMin,
+
+            //* Bottom face
+            // Top left
+            xMin,yMin,
+            // Bottom left
+            xMin,yMax,
+            // Top right
+            xMax,yMax,
+            // Bottom right
+            xMax,yMin,
+        ];
+    }
+
+    int[] getBones() {
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }
+
+    //!====================== END OPENGL METHODS =================================
+
+    //*====================== BEGIN JSON METHODS =================================
+
     void loadBlocks(string fileLocation) {
         if (!exists(fileLocation)) {
             throw new Exception(fileLocation ~ " does not exist!");
@@ -199,150 +352,7 @@ class BlockModel {
         }
     }
 
-    // void constructCube(double width, double height, double length) {
 
-    //     this.size.x = width;
-    //     this.size.y = height;
-    //     this.size.z = length;
-
-    //     this.constructVertexPositions();
-    //     this.constructIndices();
-    // }
-
-    // void constructVertexPositions() {
-        
-    //     // 8 Vertex Positions
-
-    //     // Wall 1 (FRONT)
-    //     const auto v0 = Vector3d(size.x, size.y * 2, -size.z);
-    //     const auto v1 = Vector3d(size.x, 0, -size.z);
-    //     const auto v2 = Vector3d(-size.x, 0, -size.z);
-    //     const auto v3 = Vector3d(-size.x, size.y * 2, -size.z);
-
-    //     // Wall 2 (BACK)
-    //     const auto v4 = Vector3d(-size.x, size.y * 2, size.z);
-    //     const auto v5 = Vector3d(-size.x, 0, size.z);
-    //     const auto v6 = Vector3d(size.x, 0, size.z);
-    //     const auto v7 = Vector3d(size.x, size.y * 2, size.z);
-
-    //     // Front face
-    //     assembleQuad(v0,v1,v2,v3);
-    //     //Back face
-    //     assembleQuad(v4,v5,v6,v7);
-
-    //     //Left face
-    //     assembleQuad(v3,v2,v5,v4);
-    //     //Right face
-    //     assembleQuad(v7,v6,v1,v0);
-
-    //     // Top face (up is -Z, points to front face)
-    //     assembleQuad(v3,v4,v7,v0);
-    //     // Bottom face (up is -Z, points to front face)
-    //     assembleQuad(v1,v6,v5,v2);
-
-        
-    // }
-
-    // // Builds a plane of 2 tris out of 4 vertex positions
-    // void assembleQuad(Vector3d pos1, Vector3d pos2, Vector3d pos3, Vector3d pos4) {
-    //     foreach (thisVertexPos; [pos1, pos2, pos3, pos4]) {
-    //         vertexPositions ~= thisVertexPos.x;
-    //         vertexPositions ~= thisVertexPos.y;
-    //         vertexPositions ~= thisVertexPos.z;
-    //     }
-    // }
-
-    // // Assembles the indices of the block
-    // void constructIndices() {
-
-    //     const int currentCount = cast(int)indices.length;
-
-    //     foreach (int key; indiceOrder) {
-    //         indices ~= currentCount + key;
-    //     }
-
-    //     writeln(this.indices);
-    // }
-
-    // float[] getVertexPositions() {
-    //     return this.vertexPositions;
-    // }
-
-    // int[] getIndices() {
-    //     return this.indices;
-    // }
-
-    // float[] getTextureCoordinates() {
-    //     // These are place holders for future modeling implementation
-    //     const float xMin = 0.0;
-    //     const float xMax = 1.0;
-    //     const float yMin = 0.0;
-    //     const float yMax = 1.0;
-    //     return [
-    //         //* Front face
-    //         // Top left
-    //         xMin,yMin,
-    //         // Bottom left
-    //         xMin,yMax,
-    //         // Top right
-    //         xMax,yMax,
-    //         // Bottom right
-    //         xMax,yMin,
-
-    //         //* Back face
-    //         // Top left
-    //         xMin,yMin,
-    //         // Bottom left
-    //         xMin,yMax,
-    //         // Top right
-    //         xMax,yMax,
-    //         // Bottom right
-    //         xMax,yMin,
-
-    //         //* Left face
-    //         // Top left
-    //         xMin,yMin,
-    //         // Bottom left
-    //         xMin,yMax,
-    //         // Top right
-    //         xMax,yMax,
-    //         // Bottom right
-    //         xMax,yMin,
-
-    //         //* Right face
-    //         // Top left
-    //         xMin,yMin,
-    //         // Bottom left
-    //         xMin,yMax,
-    //         // Top right
-    //         xMax,yMax,
-    //         // Bottom right
-    //         xMax,yMin,
-
-    //         //* Top face
-    //         // Top left
-    //         xMin,yMin,
-    //         // Bottom left
-    //         xMin,yMax,
-    //         // Top right
-    //         xMax,yMax,
-    //         // Bottom right
-    //         xMax,yMin,
-
-    //         //* Bottom face
-    //         // Top left
-    //         xMin,yMin,
-    //         // Bottom left
-    //         xMin,yMax,
-    //         // Top right
-    //         xMax,yMax,
-    //         // Bottom right
-    //         xMax,yMin,
-    //     ];
-    // }
-
-    // int[] getBones() {
-    //     return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    // }
+    //!====================== END JSON METHODS ===============================
     
 }
