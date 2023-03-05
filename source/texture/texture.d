8b456007f21b52f8d3783c4ae18766efed6b0e7b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-04 20:45:51 -0500

    Deshell Texture

--- source/texture/texture.d
+++ source/texture/texture.d
@@ -11,108 +11,82 @@ import tools.gl_error;
     If you add all textures in at the beginning
     of the program, this becomes a cache.
 */
-class Texture {
 
-    private static immutable GLuint invalid = GLuint.max;
+private immutable GLuint invalid = GLuint.max;
+
+// The only instance of Texture container.
+private Texture instance;
+
+// Stores all textures as simple GLuint pointers. Accessed by file location.
+private GLuint[string] storage;
+
+/// Add a texture into the container.
+GLuint addTexture(string fileLocation, bool debugEnabled = false) {
+
+    if (fileLocation in storage) {
+        throw new Exception("Attempted to create a texture twice! Must be deleted first!");
+    }
+
+    // Use ADR's awesome framework library to convert the png into a raw data stream.
+    TrueColorImage tempImageObject = readPng(fileLocation).getAsTrueColorImage();
+
+    const int width = tempImageObject.width();
+    const int height = tempImageObject.height();
+
+    ubyte[] tempData = tempImageObject.imageData.bytes;
+
+    // Now use the OpenGL framework to upload it.
+    GLuint id;
+
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tempData.ptr);
+
+    // Enable texture clamping to edge
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    // Border color is nothing
+    float[4] borderColor = [0,0,0,0];
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor.ptr);
+
+    // Add in nearest neighbor texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST/*_MIPMAP_NEAREST*/);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // glGenerateMipmap(GL_TEXTURE_2D);
+
+    GLenum glErrorInfo = getAndClearGLErrors();
+    if (glErrorInfo != GL_NO_ERROR) {
+        writeln("GL ERROR: ", glErrorInfo);
+        writeln("ERROR IN TEXTURE");
+
+        throw new Exception("Failed to load texture!");
+    }
+
+    if (debugEnabled) {
+        writeln(fileLocation, " is stored as ID: ", id);
+    }
+
+    // Finally cache the GL pointer ID as a GLuint
+    storage[fileLocation] = id;
     
-    // The only instance of Texture container.
-    private static Texture instance;
+    // Then return ID to make this even more flexible
+    return id;
+}
 
-    // Stores all textures as simple GLuint pointers. Accessed by file location.
-    private GLuint[string] storage;
-    
-    //! Note: see about making a new texture object that has a protected flag?
-    
-    private this() {}
-
-    /// Initialize the instance of the Texture class.
-    static void initialize() {
-        if (instance is null){
-            instance = new Texture();
-        }
+GLuint getTexture(string fileLocation) {
+    if (fileLocation !in storage) {
+        return invalid;
     }
-    
-    /// Add a texture into the container.
-    static GLuint addTexture(string fileLocation, bool debugEnabled = false) {
+    return storage[fileLocation];
+}
 
-        checkInstance();
+void cleanUp() {
+    foreach (string key, GLuint value; storage) {
+        glDeleteTextures(1, &value);
 
-        if (fileLocation in instance.storage) {
-            throw new Exception("Attempted to create a texture twice! Must be deleted first!");
-        }
-
-        // Use ADR's awesome framework library to convert the png into a raw data stream.
-        TrueColorImage tempImageObject = readPng(fileLocation).getAsTrueColorImage();
-
-        const int width = tempImageObject.width();
-        const int height = tempImageObject.height();
-
-        ubyte[] tempData = tempImageObject.imageData.bytes;
-
-        // Now use the OpenGL framework to upload it.
-        GLuint id;
-
-        glGenTextures(1, &id);
-        glBindTexture(GL_TEXTURE_2D, id);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tempData.ptr);
-
-        // Enable texture clamping to edge
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-        // Border color is nothing
-        float[4] borderColor = [0,0,0,0];
-        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor.ptr);
-
-        // Add in nearest neighbor texture filtering
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST/*_MIPMAP_NEAREST*/);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        // glGenerateMipmap(GL_TEXTURE_2D);
-
-        GLenum glErrorInfo = getAndClearGLErrors();
-        if (glErrorInfo != GL_NO_ERROR) {
-            writeln("GL ERROR: ", glErrorInfo);
-            writeln("ERROR IN TEXTURE");
-
-            throw new Exception("Failed to load texture!");
-        }
-
-        if (debugEnabled) {
-            writeln(fileLocation, " is stored as ID: ", id);
-        }
-
-        // Finally cache the GL pointer ID as a GLuint
-        instance.storage[fileLocation] = id;
-        
-        // Then return ID to make this even more flexible
-        return id;
+        writeln("TEXTURE ", value, " (" ~ key ~ ") HAS BEEN DELETED");
     }
-
-    static GLuint getTexture(string fileLocation) {
-        checkInstance();
-
-        if (fileLocation !in instance.storage) {
-            return invalid;
-        }
-        return instance.storage[fileLocation];
-    }
-
-    static void cleanUp() {
-        checkInstance();
-
-        foreach (string key, GLuint value; instance.storage) {
-            glDeleteTextures(1, &value);
-
-            writeln("TEXTURE ", value, " (" ~ key ~ ") HAS BEEN DELETED");
-        }
-    }
-
-    private static void checkInstance() {
-        if(instance is null) {
-            throw new Exception("Texture was never initialized!");
-        }
-    }
-
 }
