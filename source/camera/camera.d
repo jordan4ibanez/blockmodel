388061c7e9b7022eb5199f74139de35cba7f8cd4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-04 19:50:41 -0500

    Now begin turning camera into a singleton

--- source/camera/camera.d
+++ source/camera/camera.d
@@ -8,8 +8,8 @@ import math;
 
 class Camera {
 
-    // There can only be one camera in the game, this is it
-    private static bool locked = false;
+    
+    private static Camera instance;
     
 
     private double FOV = math.toRadians(60.0);
@@ -27,12 +27,7 @@ class Camera {
     private Vector3d position = Vector3d(0,0,0);
     private Vector3d rotation = Vector3d(0,0,0); 
 
-    this() {
-        if (locked) {
-            throw new Exception("Tried to add more than one camera!");
-        }
-        locked = true;
-    }
+    private this() {}
 
     Matrix4d getCameraMatrix() {
         return cameraMatrix;
