47dfa94282a2bde1002c1e41553ece6f60628ce1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-04 23:49:33 -0500

    Give camera an ortholinear matrix

--- source/camera/camera.d
+++ source/camera/camera.d
@@ -2,6 +2,7 @@ module camera.camera;
 
 import bindbc.opengl;
 import Window = window.window;
+import vector_2d;
 import vector_3d;
 import matrix_4d;
 import math;
@@ -17,6 +18,7 @@ private immutable double Z_NEAR = 0.1;
 private immutable double Z_FAR = 600.0;
 
 private Matrix4d cameraMatrix = Matrix4d();
+private Matrix4d guiMatrix    = Matrix4d();
 private Matrix4d objectMatrix = Matrix4d();
 
 // Set at x:0, y:0 z:0 so I can see the "center of the 4d world"
@@ -25,6 +27,10 @@ private Vector3d rotation = Vector3d(0,0,0);
 
 Matrix4d getCameraMatrix() {
     return cameraMatrix;
+}
+
+Matrix4d getGuiMatrix() {
+    return guiMatrix;
 }
 
 Matrix4d getObjectMatrix() {
@@ -50,6 +56,15 @@ float[16] setObjectMatrix(Vector3d offset, Vector3d rotation, Vector3d scale) {
         .scale(scale);
     return objectMatrix.getFloatArray();
 }
+// ^ v Both of these functions reuse the object matrix
+float[16] setGuiObjectMatrix(Vector2d offset) {
+
+    // The primary usecase for this is GUI.
+    objectMatrix
+        .identity()
+        .translate(offset.x, offset.y, 0.0);
+    return objectMatrix.getFloatArray();
+}
 
 /**
 This is where the camera gets it's viewpoint for the frame
@@ -65,6 +80,14 @@ float[16] updateCameraMatrix() {
         .perspective(FOV, aspectRatio, Z_NEAR, Z_FAR)
         .rotateX(math.toRadians(rotation.x))
         .rotateY(math.toRadians(rotation.y));
+    return cameraMatrix.getFloatArray();
+}
+
+float[16] updateGuiMatrix() {
+    const float width = Window.getWidth / 2.0;
+    const float height = Window.getHeight / 2.0;
+    cameraMatrix.identity()
+        .setOrtho2D(-width, width, height, -height);
     return cameraMatrix.getFloatArray();
 }
 
