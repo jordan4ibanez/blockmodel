eea907c1f2697d38db6ca8c4605f23290d9ce354
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-07 01:27:47 -0500

    Add some bare bones

--- source/gui/razor_font.d
+++ source/gui/razor_font.d
@@ -1,11 +1,46 @@
 module gui.razor_font;
 
+//  ____________________________
+// |         RAZOR FONT         |
+// |____________________________|
+//  \            /\            /
+//  /            \/            \
+// | The Sharpest Font Library  |
+// |   For D Game Development   |
+// |____________________________|
 
 
-private RazorFont[] razorFonts;
+/**
+Spec:
+
+File format:
+1. PNG with accompanying JSON in same directory. If these requirements are not met, it will throw an exception.
+
+*/
+
+private RazorFont[string] razorFonts;
 
 // A simple font container
 private class RazorFont {
-    int width  = 0;
-    int height = 0;
+
+    // Font base pallet width
+    int palletWidth  = 0;
+    int palletHeight = 0;
+
+    // Pixel space (literally) between characters in pallet
+    int border = 0;
+
+    // Number of characters
+    int characterWidth  = 0;
+    int characterHeight = 0;
+
+    // Character pallet (individual) in pixels
+    int characterPalletWidth  = 0;
+    int characterPalletHeight = 0;
+    
+    // Readonly specifier if kerning was enabled
+    bool kerned = false;
+
+    // Readonly specifier if trimming was enabled
+    bool trimmed = false;
 }
