ff3dd989041057a04c05fba69905561faaed3a94
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-07 06:06:30 -0500

    Add some ridiculous story

--- source/app.d
+++ source/app.d
@@ -157,7 +157,7 @@ void main()
 
         Window.clear(1);
 
-        if (false) {
+        if (true) {
             Camera.clearDepthBuffer();
             Camera.setRotation(Vector3d(0,0,0));
 
@@ -223,7 +223,9 @@ void main()
 
             Shader.setUniformMatrix4f("2d", "cameraMatrix", Camera.updateGuiMatrix());
 
-            const Tuple!(double[], "vertexData", double[], "textureData", int[], "indices") test = Font.debugRenderDouble("cool", 50, "i am debug! fps: " ~ to!string(Window.getFPS));
+            const Tuple!(double[], "vertexData", double[], "textureData", int[], "indices") test = Font.debugRenderDouble("cool", 50, "this is a performance test\ncurrently doing multiline\ncurrent fps: " ~ to!string(Window.getFPS) ~
+                "\ni ran out of room on this\nso i had to go to the next line\nbut hopefully you can read all this\ndid i ever tell you about the time\nthat i caught a fish thiiiiiis\nbig"~
+                "it was huge dude, literally could barely\npull the dang thing out of the water");
 
             Mesh myCoolText = new Mesh()
                 .addVertices2d(to!(float[])(test.vertexData))
