9b9ea0f2bddb4735b22f150f1a981df1e1e83a69
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-17 17:37:53 -0400

    Overhaul

---
+++ source/tools/opengl_error_logger.d
@@ -0,0 +1,93 @@
+module tools.opengl_error_logger;
+
+import std.stdio;
+import std.conv: to;
+
+import bindbc.opengl;
+
+
+// Utilizes builder pattern
+
+/// This is a wrapper class to emulate Vulkan style error toolchains
+class OpenGLErrorLogger {
+
+    GLenum error;
+
+    private string helperTip = null;
+    private string openGlErrorType = null;
+    private string openGlErrorMessage = null;
+
+    this() {
+        this.getAndClearOpenGLErrors();
+    }
+
+
+    //** ----- BEGIN LOGGER TOOLS -----
+
+    /// Allows inserting helpful tips into the error message
+    OpenGLErrorLogger attachTip(string helperTip) {
+        this.helperTip = helperTip;
+
+        return this;
+    }
+
+    /// Attaches the OpenGL error type
+    OpenGLErrorLogger attachType(const(char)* openGlErrorType) {
+
+        this.openGlErrorType = to!string(openGlErrorType);
+
+        return this;
+    }
+
+    /// Attaches the OpenGL error message
+    OpenGLErrorLogger attachMessage(const(char)* openGlErrorMessage) {
+
+        this.openGlErrorMessage = to!string(openGlErrorMessage);
+
+        return this;
+    }
+
+    /**
+    Put this at the end of the chain or else it does nothing.
+    Having this as a separate function is useful for auto clearing on OpenGL context creation.
+    */
+    void execute() {
+
+        // Accumulate it into a nice error log tailored EXACTLY to D's exception style
+
+        string messageAccumulator = "OpenGL ERROR!\n";
+
+        // writeln("OpenGL: ", , "!\n: ", );
+
+    }
+
+
+    //!! ----- END LOGGER TOOLS -----
+
+    //** ----- BEGIN OPENGL INTERFACE -----
+
+    /**
+    Automates the capture and reporting of OpenGL errors.
+    This is very useful for OpenGL context creation.
+    */
+    private void getAndClearOpenGLErrors(){
+
+        writeln("getting and clearing");
+
+        this.error = glGetError();
+
+        // Clear OpenGL errors
+        while (glGetError() != GL_NO_ERROR) {
+            glGetError();
+        }
+    }
+
+    //!! ----- END OPENGL INTERFACE -----
+
+}
+
+/// This is an implicitly named variable to clear out the context of opengl
+void clearOpenGLErrors() {
+    // You can see this can just be instantiated without anything because it automatically performs it's task
+    new OpenGLErrorLogger();
+}
