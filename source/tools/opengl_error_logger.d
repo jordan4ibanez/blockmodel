e7c9c987ce404b691eaa5b07b37f9d5354ac85b0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-17 18:30:30 -0400

    Overhaul GL error logger

--- source/tools/opengl_error_logger.d
+++ source/tools/opengl_error_logger.d
@@ -15,6 +15,8 @@ class OpenGLErrorLogger {
     // This is just a fancy uint
     GLenum error;
 
+    private string accumulator = "OpenGL ERROR!\n";
+
     private string helperTip = null;
 
     this() {
@@ -31,6 +33,33 @@ class OpenGLErrorLogger {
         return this;
     }
 
+    private string grabReadableErrorInfo() {
+        /**
+        This is from: https://learnopengl.com/In-Practice/Debugging
+
+        I Highly recommend visiting this site, it's really good :)
+        */
+        
+        switch (error) {
+            case GL_INVALID_ENUM:                  return "INVALID_ENUM";
+            case GL_INVALID_VALUE:                 return "INVALID_VALUE";
+            case GL_INVALID_OPERATION:             return "INVALID_OPERATION";
+            case GL_STACK_OVERFLOW:                return "STACK_OVERFLOW";
+            case GL_STACK_UNDERFLOW:               return "STACK_UNDERFLOW";
+            case GL_OUT_OF_MEMORY:                 return "OUT_OF_MEMORY";
+            case GL_INVALID_FRAMEBUFFER_OPERATION: return "INVALID_FRAMEBUFFER_OPERATION";
+            // Just pray you never see an unknown error in debugging I guess
+            default:                               return "UNKNOWN";
+        }
+
+    }
+
+    // Literally just inserts line seperators into the string accumulator
+    private void line() {
+        this.accumulator ~= "========================================";
+    }
+
+
     /**
     Put this at the end of the chain or else it does nothing.
     Having this as a separate function is useful for auto clearing on OpenGL context creation.
@@ -41,8 +70,18 @@ class OpenGLErrorLogger {
 
         string messageAccumulator = "OpenGL ERROR!\n";
 
-        // writeln("OpenGL: ", , "!\n: ", );
+        line();
 
+        accumulator ~= "Direct Error Code: " ~ grabReadableErrorInfo() ~ "\n";
+
+        line();
+
+        if (helperTip !is null) {
+            accumulator ~= helperTip ~ "\n";
+        }
+
+        // Prints it out as a nice helpful message
+        throw new Exception(accumulator);
     }
 
 
