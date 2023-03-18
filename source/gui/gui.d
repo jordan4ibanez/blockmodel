6e72a79f1723a251209b4e65a450915beb44df74
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-17 20:31:07 -0400

    Add a bunch more framework

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -6,22 +6,38 @@ import doml.vector_2d;
 import doml.vector_4d;
 
 import Font = razor_font;
+import Texture = texture.texture;
+import Window = window.window;
+
 
 // Allows elements to be bolted to a part of the window
-enum WINDOW_POSITION {
+enum WINDOW_POSITION : Vector2d {
 
-    TOP_LEFT,
-    TOP_CENTER,
-    TOP_RIGHT,
+    TOP_LEFT      = Vector2d( 0.0, 0.0 ),
+    TOP_CENTER    = Vector2d( 0.5, 0.0 ),
+    TOP_RIGHT     = Vector2d( 1.0, 0.0 ),
 
-    CENTER_LEFT,
-    CENTER_CENTER,
-    CENTER_RIGHT,
+    CENTER_LEFT   = Vector2d( 0.0, 0.5 ),
+    CENTER_CENTER = Vector2d( 0.5, 0.5 ),
+    CENTER_RIGHT  = Vector2d( 1.0, 0.5 ),
 
-    BOTTOM_LEFT,
-    BOTTOM_CENTER,
-    BOTTOM_RIGHT
+    BOTTOM_LEFT   = Vector2d( 0.0, 1.0 ),
+    BOTTOM_CENTER = Vector2d( 0.5, 1.0 ),
+    BOTTOM_RIGH   = Vector2d( 1.0, 1.0 )
 
+}
+
+// Default the position to top left
+immutable WINDOW_POSITION DEFAULT = WINDOW_POSITION.TOP_LEFT;
+
+/// This auto converts the enumerator into a direct position on screen
+Vector2d grabWindowPosition(WINDOW_POSITION inputPosition) {
+    Vector2d windowSize = Window.getSize();
+    
+    return Vector2d(
+        inputPosition.x * windowSize.x,
+        inputPosition.y * windowSize.y
+    );
 }
 
 class GUI {
@@ -51,6 +67,8 @@ class GUI {
 
 class Button {
 
+    WINDOW_POSITION windowPosition = DEFAULT;
+
     private GLuint backgroundTexture;
     private Text text;
 
@@ -66,6 +84,10 @@ class Text {
 
     double size = 0;
 
+    // If the text is within a button, this will be ignored
+    WINDOW_POSITION windowPosition = DEFAULT;
+
+    // This is the position relative to the window position
     Vector2d position;
 
     Vector4d color = Vector4d(0,0,0,1);
@@ -76,8 +98,15 @@ class Text {
         this.textData = textData;
     }
 
+    /// This is the position relative to the window position
     Text setPosition(Vector2d position) {
         this.position = position;
+        return this;
+    }
+
+    /// This is the position in the window where the text will be bolted to
+    Text setWindowPosition(WINDOW_POSITION windowPosition) {
+        this.windowPosition = windowPosition;
         return this;
     }
 
