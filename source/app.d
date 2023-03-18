eb415fb6bfe7917c00ed98a3b0ce160ddf2aeed4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-17 20:13:29 -0400

    Start this

--- source/app.d
+++ source/app.d
@@ -14,6 +14,8 @@ import Math = doml.math;
 import delta_time;
 import std.typecons;
 
+import gui.gui;
+
 //! Development import REMOVE LATER
 import Font = razor_font;
 
@@ -27,8 +29,6 @@ void main()
     Texture.addTexture("textures/xyz_compass.png");
     Texture.addTexture("textures/debug_character.png");
     Texture.addTexture("textures/debug.png");
-
-    //! Start Razor Font testing
 
 
     Font.setRenderTargetAPICallString(
@@ -56,15 +56,7 @@ void main()
     );
     
     Font.createFont("fonts/totally_original", "mc", true);
-
-
-    //* End Razor Font testing
-
-    // bool blah = true;
-    // if (blah) {
-    //     Window.destroy();
-    //     return;
-    // }
+    
     
     
     // Controls blockmodel rendering
@@ -152,11 +144,28 @@ void main()
 
     Window.setVsync(0);
 
+
+    //! GUI TESTING
+
+
+    GUI gui = new GUI();
+
+    Font.selectFont("mc");
+
+    gui.addText("cool",
+        new Text("cool").setPosition(Vector2d(0,0)).setSize(20)
+    );
+
+
+    //! END GUI TESTING
+
+
     while (!Window.shouldClose()) {
         // Poll events is hugging the entry point to the scope
         // because it needs to take all GLFW input before anything
         // is calculated. This increases responsiveness.
         Window.pollEvents();
+
 
         fancyRotation += getDelta * 100.0;
         if (fancyRotation >= 360.0) {
@@ -167,63 +176,19 @@ void main()
 
         Window.clear(0.9);
 
-        if (false) {
-            Camera.clearDepthBuffer();
-            Camera.setRotation(Vector3d(0,0,0));
+        Camera.clearDepthBuffer();
+        Camera.setRotation(Vector3d(0,0,0));
 
-            // Render sam first
+        Shader.setUniformMatrix4("2d", "cameraMatrix", Camera.updateGuiMatrix());
+        Shader.setUniformMatrix4("2d", "objectMatrix", Camera.setGuiObjectMatrix(Vector2d(0,0)) );
 
-            Shader.startProgram("model");
+        Shader.startProgram("2d");
 
-            Shader.setUniformMatrix4("model", "boneTRS", model.playAnimation(1), model.total_blocks);
-            Shader.setUniformMatrix4("model", "cameraMatrix", Camera.updateCameraMatrix());
+        Font.setCanvasSize(Window.getWidth(), Window.getHeight());
 
-            Shader.setUniformMatrix4("model", "objectMatrix",
-                Camera.setObjectMatrix(
-                    Vector3d(0,-3,-10), // Translation
-                    Vector3d(0,fancyRotation,0), // Rotation
-                    Vector3d(1), // Scale
-                )
-            );
+        gui.render();
 
-            debugMesh.render("model");
-
-            // Render the xyz compass on top
-            Camera.clearDepthBuffer();
-
-            Shader.startProgram("regular");
-
-            Shader.setUniformMatrix4("regular", "cameraMatrix", Camera.updateCameraMatrix());
-
-            Shader.setUniformMatrix4("regular", "objectMatrix",
-                Camera.setObjectMatrix(
-                    Vector3d(0,-1,-4), // Translation
-                    Vector3d(0,fancyRotation,0), // Rotation
-                    Vector3d(1), // Scale
-                )
-            );
-
-            xyzCompass.render("regular");
-
-
-            // Now render this debug thing on top of that
-            Camera.clearDepthBuffer();
-
-            Shader.startProgram("2d");
-
-            Shader.setUniformMatrix4("2d", "cameraMatrix", Camera.updateGuiMatrix());
-
-            Shader.setUniformMatrix4("2d", "objectMatrix",
-                Camera.setGuiObjectMatrix(
-                    Vector2d(
-                        (Window.getWidth / 2.0) - debug2d.getWidth,
-                        (Window.getHeight / 2.0) - debug2d.getHeight
-                    )
-                )
-            );
-
-            debug2d.render("2d");
-        }
+        
 
         // if (true) {
         //     // Now render this font
