8bc0a89a765818ccd2821614aeabec182383577a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-23 19:20:48 -0400

    Duct tape this on

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -276,17 +276,223 @@ class SpreadSheet {
     private Button[][] buttons;
     
     Vector2d size;
+
+    // The background
+    Mesh mesh = null;
     
-    SpreadSheet setSize(double width, double height) {
+    this(double width, double height) {
         this.size = Vector2d(width, height);
-        return this;
+        this.generateMesh();
     }
 
+    private void generateMesh() {
+        if (mesh is null) {
+            this.mesh.cleanUp();
+        }
 
-    
+        // The guide edges for buttons, keeps texture edges from stretching
+        // So think of this of like: How many pixels does your button texture use before getting to the text part.
+        immutable double pixelEdge = 1.0;
+        // Border scalar just makes the button border more pronounced/visible
+        immutable double borderScalar = 2.0;
 
+        Vector2d textureSize = Texture.getTextureSize("textures/button.png");
 
+        // Accumulates the mesh data for the button
+        double[] vertices;
+        double[] textureCoords;
+        int[] indices;
 
+        // We're going to use the height to create the consistent layout
+
+        double centerBorder = (size.y / textureSize.y) * pixelEdge * borderScalar;
+
+        /**
+        This is each point on the horizontal 1d array of the button background.
+        
+        0  1                                 2  3
+         _______________________________________
+        |  ___________________________________  |
+        | |                                   | |
+
+        */
+
+        //                                  0  1             2                      3
+        const double[4] horizontalVertex = [0, centerBorder, size.x - centerBorder, size.x];
+
+        /**
+        This is each point on the vertical 1d array of button background.
+
+        0  ________
+          | 
+        1 |    ____
+          |   |
+          |   |
+          |   |
+        2 |   |_____
+          |
+        3 |_________
+
+        */
+
+        //                                0  1             2                      3
+        const double[4] verticalVertex = [0, centerBorder, size.y - centerBorder, size.y];
+        
+        vertices ~= [
+
+            // Top left
+            horizontalVertex[0], verticalVertex[0],
+            horizontalVertex[0], verticalVertex[1],
+            horizontalVertex[1], verticalVertex[1],
+            horizontalVertex[1], verticalVertex[0],
+
+            // Top center
+            horizontalVertex[1], verticalVertex[0],
+            horizontalVertex[1], verticalVertex[1],
+            horizontalVertex[2], verticalVertex[1],
+            horizontalVertex[2], verticalVertex[0],
+
+            // Top right
+            horizontalVertex[2], verticalVertex[0],
+            horizontalVertex[2], verticalVertex[1],
+            horizontalVertex[3], verticalVertex[1],
+            horizontalVertex[3], verticalVertex[0],
+
+            // Center left
+            horizontalVertex[0], verticalVertex[1],
+            horizontalVertex[0], verticalVertex[2],
+            horizontalVertex[1], verticalVertex[2],
+            horizontalVertex[1], verticalVertex[1],
+
+            // Center center
+            horizontalVertex[1], verticalVertex[1],
+            horizontalVertex[1], verticalVertex[2],
+            horizontalVertex[2], verticalVertex[2],
+            horizontalVertex[2], verticalVertex[1],
+
+            // Center right
+            horizontalVertex[2], verticalVertex[1],
+            horizontalVertex[2], verticalVertex[2],
+            horizontalVertex[3], verticalVertex[2],
+            horizontalVertex[3], verticalVertex[1],
+
+            // Bottom left
+            horizontalVertex[0], verticalVertex[2],
+            horizontalVertex[0], verticalVertex[3],
+            horizontalVertex[1], verticalVertex[3],
+            horizontalVertex[1], verticalVertex[2],
+
+            // Bottom center
+            horizontalVertex[1], verticalVertex[2],
+            horizontalVertex[1], verticalVertex[3],
+            horizontalVertex[2], verticalVertex[3],
+            horizontalVertex[2], verticalVertex[2],
+
+            // Bottom right
+            horizontalVertex[2], verticalVertex[2],
+            horizontalVertex[2], verticalVertex[3],
+            horizontalVertex[3], verticalVertex[3],
+            horizontalVertex[3], verticalVertex[2],
+        ];
+
+        /**
+        So the texture coordinates work exactly as explained above, only we're mapping to the texture
+        instead of generating the vertices.
+        */
+
+        //                                   0    1                          2                                            3
+        const double[4] horizontalTexture = [0.0, pixelEdge / textureSize.x, (textureSize.x - pixelEdge) / textureSize.x, 1.0];
+        
+
+        //                                 0    1                          2                                            3
+        const double[4] verticalTexture = [0.0, pixelEdge / textureSize.y, (textureSize.y - pixelEdge) / textureSize.y, 1.0];
+        textureCoords ~= [
+            // Top left
+            horizontalTexture[0], verticalTexture[0],
+            horizontalTexture[0], verticalTexture[1],
+            horizontalTexture[1], verticalTexture[1],
+            horizontalTexture[1], verticalTexture[0],
+
+            // Top center
+            horizontalTexture[1], verticalTexture[0],
+            horizontalTexture[1], verticalTexture[1],
+            horizontalTexture[2], verticalTexture[1],
+            horizontalTexture[2], verticalTexture[0],
+
+            // Top right
+            horizontalTexture[2], verticalTexture[0],
+            horizontalTexture[2], verticalTexture[1],
+            horizontalTexture[3], verticalTexture[1],
+            horizontalTexture[3], verticalTexture[0],
+
+            // Center left
+            horizontalTexture[0], verticalTexture[1],
+            horizontalTexture[0], verticalTexture[2],
+            horizontalTexture[1], verticalTexture[2],
+            horizontalTexture[1], verticalTexture[1],
+
+            // Center center
+            horizontalTexture[1], verticalTexture[1],
+            horizontalTexture[1], verticalTexture[2],
+            horizontalTexture[2], verticalTexture[2],
+            horizontalTexture[2], verticalTexture[1],
+
+            // Center right
+            horizontalTexture[2], verticalTexture[1],
+            horizontalTexture[2], verticalTexture[2],
+            horizontalTexture[3], verticalTexture[2],
+            horizontalTexture[3], verticalTexture[1],
+
+            // Bottom left
+            horizontalTexture[0], verticalTexture[2],
+            horizontalTexture[0], verticalTexture[3],
+            horizontalTexture[1], verticalTexture[3],
+            horizontalTexture[1], verticalTexture[2],
+
+            // Bottom center
+            horizontalTexture[1], verticalTexture[2],
+            horizontalTexture[1], verticalTexture[3],
+            horizontalTexture[2], verticalTexture[3],
+            horizontalTexture[2], verticalTexture[2],
+
+            // Bottom right
+            horizontalTexture[2], verticalTexture[2],
+            horizontalTexture[2], verticalTexture[3],
+            horizontalTexture[3], verticalTexture[3],
+            horizontalTexture[3], verticalTexture[2],
+        ];
+        
+        indices ~= [
+            // Top left
+            0,1,2,2,3,0,
+            // Top center
+            4,5,6,6,7,4,
+            // Top right
+            8,9,10,10,11,8,
+            // Center left
+            12,13,14,14,15,12,
+            // Center center
+            16,17,18,18,19,16,
+            // Center right
+            20,21,22,22,23,20,
+            // Bottom left
+            24,25,26,26,27,24,
+            // Bottom center
+            28,29,30,30,31,28,
+            // Bottom right
+            32,33,34,34,35,32
+        ];
+
+        this.mesh = new Mesh()
+            .addVertices2d(vertices)
+            .addTextureCoordinates(textureCoords)
+            .addIndices(indices)
+            // This is hardcoded for now
+            .setTexture(Texture.getTexture("textures/button.png"))
+            .finalize();
+
+        
+    }
 
 }
 
